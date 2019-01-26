83808cfee451552c625dbbacc19aa9e3ed606304
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-01-26 16:35:18 +0300

    use beforeEach and AfterEach in hashtable's tests

--- 1/ru.hse.anstkras/src/test/java/ru/hse/anstkras/hashtable/HashTableTest.java
+++ 1/ru.hse.anstkras/src/test/java/ru/hse/anstkras/hashtable/HashTableTest.java
@@ -1,27 +1,37 @@
 package ru.hse.anstkras.hashtable;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HashTableTest {
+    private HashTable hashTable;
+
+    @BeforeEach
+    void init() {
+        hashTable = new HashTable();
+    }
+
+    @AfterEach
+    void makeNull() {
+        hashTable = null;
+    }
 
     @Test
     void empty() {
-        HashTable hashTable = new HashTable();
         assertTrue(hashTable.empty());
     }
 
     @Test
     void notEmpty() {
-        HashTable hashTable = new HashTable();
         hashTable.put("key0", "value0");
         assertFalse(hashTable.empty());
     }
 
     @Test
     void size() {
-        HashTable hashTable = new HashTable();
         hashTable.put("key0", "value0");
         hashTable.put("key1", "value1");
         assertEquals(2, hashTable.size());
@@ -29,7 +39,6 @@ class HashTableTest {
 
     @Test
     void contains() {
-        HashTable hashTable = new HashTable();
         for (int i = 0; i < 50; i++) {
             hashTable.put("key" + i, "value" + i);
         }
@@ -38,7 +47,6 @@ class HashTableTest {
 
     @Test
     void get() {
-        HashTable hashTable = new HashTable();
         for (int i = 0; i < 50; i++) {
             hashTable.put("key" + i, "value" + i);
         }
@@ -47,7 +55,6 @@ class HashTableTest {
 
     @Test
     void getNull() {
-        HashTable hashTable = new HashTable();
         for (int i = 0; i < 50; i++) {
             hashTable.put("key" + i, "value" + i);
         }
@@ -56,7 +63,6 @@ class HashTableTest {
 
     @Test
     void putIdenticalKeys() {
-        HashTable hashTable = new HashTable();
         hashTable.put("123", "1");
         hashTable.put("123", "2");
         assertEquals("2", hashTable.get("123"));
@@ -64,7 +70,6 @@ class HashTableTest {
 
     @Test
     void put() {
-        HashTable hashTable = new HashTable();
         for (int i = 0; i < 42; i++) {
             hashTable.put("key" + Integer.toString(i), "value" + Integer.toString(i));
         }
@@ -73,7 +78,6 @@ class HashTableTest {
 
     @Test
     void remove() {
-        HashTable hashTable = new HashTable();
         for (int i = 0; i < 50; i++) {
             hashTable.put("key" + i, "value" + i);
         }
@@ -83,7 +87,6 @@ class HashTableTest {
 
     @Test
     void removeNull() {
-        HashTable hashTable = new HashTable();
         for (int i = 0; i < 50; i++) {
             hashTable.put("key" + i, "value" + i);
         }
@@ -92,7 +95,6 @@ class HashTableTest {
 
     @Test
     void clear() {
-        HashTable hashTable = new HashTable();
         for (int i = 0; i < 100; i++) {
             hashTable.put(Integer.toString(i), Integer.toString(i));
         }
