e337d5f58e35973f25cfde71df6b815ec48ca462
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-01-27 19:29:47 +0300

    add public modifier to the interface

--- 2/src/main/java/Serializable/Serializable.java
+++ 2/src/main/java/Serializable/Serializable.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-interface Serializable {
+public interface Serializable {
     void serialize(OutputStream out) throws IOException;
 
     void deserialize(InputStream in) throws IOException;
