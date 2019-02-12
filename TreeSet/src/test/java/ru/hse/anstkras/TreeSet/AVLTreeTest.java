1779b81287f9331065f26c60273a391047edc660
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-02-12 15:15:37 +0300

    add tests for a tree with comparator

--- TreeSet/src/test/java/ru/hse/anstkras/TreeSet/AVLTreeTest.java
+++ TreeSet/src/test/java/ru/hse/anstkras/TreeSet/AVLTreeTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AVLTreeTest {
+
+    private final Integer[] integerArray = {-10, 4, 1, 100, 2, -21, 88, 32, 51, 23, 12};
+    private final String[] stringArray = {"eQD", "eqA", "e", "Fsd", "ABC", "AbD", "EQ"};
 
     @Test
     void addTheSameValue() {
@@ -141,11 +145,105 @@ class AVLTreeTest {
         assertEquals(4, (int) tree.floor(11));
     }
 
-    private Integer[] integerArray = {-10, 4, 1, 100, 2, -21, 88, 32, 51, 23, 12};
+    @Test
+    void descendingSetIterator() {
+        AVLTree<Integer> tree = getIntegerFilledTree();
+        Iterator<Integer> descendingIterator = tree.descendingIterator();
+        Iterator<Integer> descendingTreeIterator = tree.descendingSet().iterator();
+        while (descendingIterator.hasNext() && descendingTreeIterator.hasNext()) {
+            assertEquals(descendingIterator.next(), descendingTreeIterator.next());
+        }
+        assertFalse(descendingIterator.hasNext());
+        assertFalse(descendingTreeIterator.hasNext());
+    }
+
+    @Test
+    void descendingSetDescendingIterator() {
+        AVLTree<Integer> tree = getIntegerFilledTree();
+        Iterator<Integer> iterator = tree.iterator();
+        Iterator<Integer> descendingTreeDescendingIterator = tree.descendingSet().descendingIterator();
+        while (iterator.hasNext() && descendingTreeDescendingIterator.hasNext()) {
+            assertEquals(iterator.next(), descendingTreeDescendingIterator.next());
+        }
+        assertFalse(iterator.hasNext());
+        assertFalse(descendingTreeDescendingIterator.hasNext());
+    }
+
+    @Test
+    void descendingSetDescendingSet() {
+        MyTreeSet<Integer> tree = getIntegerFilledTree();
+        MyTreeSet<Integer> twiceDescendingTree = tree.descendingSet().descendingSet();
+        Integer[] treeArray = new Integer[tree.size()];
+        tree.toArray(treeArray);
+        Integer[] twiceDescendingTreeArray = new Integer[twiceDescendingTree.size()];
+        twiceDescendingTree.toArray(twiceDescendingTreeArray);
+        assertArrayEquals(treeArray, twiceDescendingTreeArray);
+    }
+
+    @Test
+    void descendingSetFirst() {
+        MyTreeSet<Integer> tree = getIntegerFilledTree().descendingSet();
+        assertEquals(100, (int) tree.first());
+    }
+
+    @Test
+    void descendingSetLast() {
+        MyTreeSet<Integer> tree = getIntegerFilledTree().descendingSet();
+        assertEquals(-21, (int) tree.last());
+    }
+
+    @Test
+    void descendingSetLower() {
+        MyTreeSet<Integer> tree = getIntegerFilledTree().descendingSet();
+        assertEquals(32, (int) tree.lower(23));
+    }
+
+    @Test
+    void descendingSetHigher() {
+        MyTreeSet<Integer> tree = getIntegerFilledTree().descendingSet();
+        assertEquals(51, (int) tree.higher(88));
+    }
+
+    @Test
+    void descendingSetCeiling() {
+        MyTreeSet<Integer> tree = getIntegerFilledTree().descendingSet();
+        assertEquals(23, (int) tree.ceiling(23));
+    }
+
+    @Test
+    void descendingSetFloor() {
+        MyTreeSet<Integer> tree = getIntegerFilledTree().descendingSet();
+        assertEquals(12, (int) tree.floor(11));
+    }
+
+    @Test
+    void firstWithComparator() {
+        AVLTree<String> tree = getStringFilledTreeWithComparator();
+        assertEquals("Fsd", tree.first());
+    }
+
+    @Test
+    void removeWithComparator() {
+        AVLTree<String> tree = getStringFilledTreeWithComparator();
+        assertTrue(tree.remove("E"));
+    }
+
+    @Test
+    void lowerWithComparator() {
+        AVLTree<String> tree = getStringFilledTreeWithComparator();
+        assertEquals("eQD", tree.lower("EQb"));
+    }
+
 
     private AVLTree<Integer> getIntegerFilledTree() {
         var tree = new AVLTree<Integer>();
         tree.addAll(Arrays.asList(integerArray));
+        return tree;
+    }
+
+    private AVLTree<String> getStringFilledTreeWithComparator() {
+        var tree = new AVLTree<>(Collections.reverseOrder(String::compareToIgnoreCase));
+        tree.addAll(Arrays.asList(stringArray));
         return tree;
     }
 }
