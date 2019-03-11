89874656984d16cebdf869115e88c6a5f13b951c
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-03-11 21:17:22 +0300

    support extends and implements

--- Reflector/src/main/java/ru/hse/anstkras/reflector/Reflector.java
+++ Reflector/src/main/java/ru/hse/anstkras/reflector/Reflector.java
@@ -17,24 +17,47 @@ public class Reflector {
     }
 
     private static void printClass(Class<?> clazz, int indent, FileWriter fileWriter) throws IOException {
-        TypeVariable<?>[] typeParameters = clazz.getTypeParameters();
-        StringJoiner params = new StringJoiner(" ,", "<", ">");
-        params.setEmptyValue("");
-        for (Type type : typeParameters) {
-            params.add(typeToString(type));
+        fileWriter.write(" ".repeat(indent));
+        fileWriter.write("public class ");
+
+        fileWriter.write(typeToString(clazz));
+
+        Class<?> superClass = clazz.getSuperclass();
+        if (superClass != null) {
+            fileWriter.write(" extends ");
+            fileWriter.write(typeToString(superClass));
         }
-        fileWriter.write(" ".repeat(indent) + "public class " + clazz.getName() + params + " {\n");
+        Class<?>[] interfaces = clazz.getInterfaces();
+        if (interfaces.length != 0) {
+            fileWriter.write(" implements ");
+            var stringJoiner = new StringJoiner(", ");
+            stringJoiner.setEmptyValue("");
+            for (Class<?> classInterface : interfaces) {
+                stringJoiner.add(typeToString(classInterface));
+            }
+            fileWriter.write(stringJoiner.toString());
+        }
+        fileWriter.write(" {\n");
+        printFields(clazz, indent, fileWriter);
+        fileWriter.write(" ".repeat(indent) + "}");
+    }
+
+    private static void printFields(Class<?> clazz, int indent, FileWriter fileWriter) throws IOException {
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
             printField(field, indent + TAB_SIZE, fileWriter);
         }
-        fileWriter.write(" ".repeat(indent) + "}");
     }
 
     private static void printField(Field field, int indent, FileWriter fileWriter) throws IOException {
         String modifiers = Modifier.toString(field.getModifiers());
         String type = field.getGenericType().getTypeName();
-        fileWriter.write(" ".repeat(indent) + modifiers + " " + type + " " + field.getName() + ";\n"); // TODO fix spaces
+        fileWriter.write(" ".repeat(indent));
+        if (!modifiers.isEmpty()) {
+            fileWriter.write(modifiers + " ");
+        }
+
+        fileWriter.write(type + " " + field.getName() + ";\n");
     }
 
     private static String typeToString(Type type) {
@@ -49,7 +72,15 @@ public class Reflector {
         } else if (type instanceof WildcardType) { // does it work?
             stringBuilder.append(type.getTypeName());
         } else if (type instanceof Class) {
-            stringBuilder.append(type.getTypeName());
+            Class<?> classType = (Class<?>) type;
+            TypeVariable<?>[] typeParameters = classType.getTypeParameters();
+            StringJoiner params = new StringJoiner(" ,", "<", ">");
+            params.setEmptyValue("");
+            for (Type typeParameter : typeParameters) {
+                params.add(typeToString(typeParameter));
+            }
+            stringBuilder.append(classType.getName());
+            stringBuilder.append(params.toString());
         } else if (type instanceof GenericArrayType) {
             var genericArrayType = (GenericArrayType) type;
             stringBuilder.append(genericArrayType.getGenericComponentType());
--- Reflector/src/test/java/ru/hse/anstkras/reflector/TestClass.java
+++ Reflector/src/test/java/ru/hse/anstkras/reflector/TestClass.java
@@ -1,13 +1,30 @@
 package ru.hse.anstkras.reflector;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import org.jetbrains.annotations.NotNull;
 
-public class TestClass<E extends List<Set<E>> & Comparable<List<? super E>>, T> {
+import java.io.Serializable;
+import java.util.*;
+
+public class TestClass<E extends List<Set<E>> & Comparable<List<? super E>>, T> extends AbstractSet<E> implements Comparable<T>, Serializable {
     private static int a;
     public volatile transient E e;
     public List<? super Collection<? extends List<? super E>>> list;
     public int[] array;
+    public List<E> liste;
     static final List<? super Collection<? extends List<? super String>>>[] arr = null;
+
+    @Override
+    public int compareTo(@NotNull T o) {
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
