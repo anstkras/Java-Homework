a4fd91ba782863c99a2c70a8a27e1ebfe6a94ca7
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-03-18 15:50:03 +0300

    add tests

--- qsort/src/test/java/ru/hse/anstkras/QSortTest.java
+++ qsort/src/test/java/ru/hse/anstkras/QSortTest.java
@@ -1,15 +1,54 @@
 package ru.hse.anstkras;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class QSortTest {
+    private QSort qSort;
+
+    @BeforeEach
+    void init() {
+        qSort = new QSort();
+    }
+
     @Test
-    void test() {
-        int[] array = {1, 2, 6, 4, 2, 1, -1, 10, 2, 4, 1, 2, 6, 7};
-        QSort qSort = new QSort();
-        qSort.quickSort(array);
-        Arrays.stream(array).forEach(System.out::println);
+    void sortEmptyArray() {
+        int[] array = {};
+        qSort.sort(array);
+        int[] expectedArray = {};
+        assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    void sortOneElement() {
+        int[] array = {1, 2};
+        qSort.sort(array);
+        int[] expectedArray = {1, 2};
+        assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    void sortSomeElements() {
+        int[] array = {1, 4, 5, 7, -100, 3, 2, 43, 43, 12, 43, 76, 12, 34, 1, 0, 1};
+        qSort.sort(array);
+        int[] expectedArray = {-100, 0, 1, 1, 1, 2, 3, 4, 5, 7, 12, 12, 34, 43, 43, 43, 76};
+        assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    void sortManyRandomElements() {
+        var random = new Random(1);
+        int[] array = random.ints(-10000, 10000).limit(10000).toArray();
+        qSort.sort(array);
+        for (int i = 0; i < array.length - 1; i++) {
+            assertTrue(array[i] <= array[i + 1]);
+        }
     }
 }
