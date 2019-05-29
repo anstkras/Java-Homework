f693d1a1b9b149c31579c98c1ab979e03eab40e6
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-05-29 17:27:49 +0300

    one more test

--- springTest2/src/test/java/ru/hse/anstkras/springtest2/BoardTest.java
+++ springTest2/src/test/java/ru/hse/anstkras/springtest2/BoardTest.java
@@ -35,4 +35,47 @@ class BoardTest {
             assertEquals(2, numbers[i]);
         }
     }
+
+    @Test
+    void testWinSmallBoard() {
+        var board = new Board(2);
+        int i1 = -1;
+        int j1 = -1;
+        int j2 = -1;
+        int i2 = -1;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (board.getCell(i, j) == 0) {
+                    if (i1 == -1) {
+                        i1 = i;
+                        j1 = j;
+                    } else {
+                        i2 = i;
+                        j2 = j;
+                    }
+                }
+            }
+        }
+        Board.MoveState moveState = board.makeMove(i1, j1,i2, j2);
+        assertEquals(Board.MoveState.SUCCESS, moveState);
+        i1 = -1;
+        j1 = -1;
+        j2 = -1;
+        i2 = -1;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (board.getCell(i, j) == 1) {
+                    if (i1 == -1) {
+                        i1 = i;
+                        j1 = j;
+                    } else {
+                        i2 = i;
+                        j2 = j;
+                    }
+                }
+            }
+        }
+        moveState = board.makeMove(i1, j1, i2, j2);
+        assertEquals(Board.MoveState.WIN, moveState);
+    }
 }
