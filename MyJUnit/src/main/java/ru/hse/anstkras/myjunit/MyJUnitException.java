1144f4ee9764e02692d9275420a8fcf9cff44d15
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-06-02 19:40:03 +0300

    make constructors package-private

--- MyJUnit/src/main/java/ru/hse/anstkras/myjunit/MyJUnitException.java
+++ MyJUnit/src/main/java/ru/hse/anstkras/myjunit/MyJUnitException.java
@@ -2,11 +2,11 @@ package ru.hse.anstkras.myjunit;
 
 /** Represents an exception that may occur during the testing */
 public class MyJUnitException extends Exception {
-    public MyJUnitException(String message) {
+    MyJUnitException(String message) {
         super(message);
     }
 
-    public MyJUnitException(String message, Throwable cause) {
+    MyJUnitException(String message, Throwable cause) {
         super(message, cause);
     }
 }
