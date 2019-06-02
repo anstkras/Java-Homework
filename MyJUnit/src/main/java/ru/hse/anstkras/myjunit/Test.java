4b15a554378ae65dd18a633b5989e3a46cc0e2f6
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-06-02 15:22:19 +0300

    init commit

---
+++ MyJUnit/src/main/java/ru/hse/anstkras/myjunit/Test.java
@@ -0,0 +1,13 @@
+package ru.hse.anstkras.myjunit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+    String ignored();
+    Class[] exception();
+}
