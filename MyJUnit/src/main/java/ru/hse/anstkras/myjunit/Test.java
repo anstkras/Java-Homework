072516864515b33d78abc280f0bb9579367bd3e3
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-06-08 13:33:04 +0300

    add docs

--- MyJUnit/src/main/java/ru/hse/anstkras/myjunit/Test.java
+++ MyJUnit/src/main/java/ru/hse/anstkras/myjunit/Test.java
@@ -13,12 +13,19 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
+    /** Default value for ignored that means that test is not ignored */
     String DEFAULT_IGNORED = "ru.hse.anstkras.myjunit.test_default_ignored";
 
+    /** If set means that the annotated as a test method should be ignored with specified reason */
     String ignored() default DEFAULT_IGNORED;
 
+    /**
+     * If set means that the annotated method must throw specified exception,
+     * otherwise the annotated method must not throw any exceptions
+     */
     Class exception() default DefaultException.class;
 
+    /** Default value for exception that means that test does not throw any exceptions */
     final class DefaultException {
     }
 }
